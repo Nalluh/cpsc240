046e0f9bcbbdab892fbe1e4f21ace8340e59a104
Nalluh <120067040+Nalluh@users.noreply.github.com>
2023-11-30 21:08:37 -0800

    Update asnn1.cpp
    deleted some junk variables that were being used for gdb testing

--- asnn1/asnn1.cpp
+++ asnn1/asnn1.cpp
@@ -8,9 +8,7 @@ extern "C" double trip_info();
 int main(){
 
   double miles = -0.00000000000000099;
-  int a = 3;
-  double b = 3.222;
-  int c[] ={1,2,3,4,5,6};
+
   cout << "Welcome to Trip Advisor by Allan Cortes. \nWe help you plan your trip.\n\n";
 
   miles = trip_info();
